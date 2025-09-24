cadcdc9e92fa4fb072e776c69fd30e3e599af180
ashutosh6603 <ashutosh6603@gmail.com>
2025-09-24 07:39:02 +0000

    solved leetcode-28

--- leetcode-28.js
+++ leetcode-28.js
@@ -17,4 +17,54 @@ Constraints:
 => haystack and needle consist of only lowercase English characters.
 */
 
-function strStr(haystack, needle) {}
+function strStr(haystack, needle) {
+  if (needle.length === 0) return 0;
+  if (haystack.length < needle.length) return -1;
+
+  const lps = buildLPS(needle);
+  let i = 0;
+  let j = 0;
+
+  while (i < haystack.length) {
+    if (haystack[i] === needle[j]) {
+      i++;
+      j++;
+    }
+
+    if (j === needle.length) {
+      return i - j;
+    } else if (i < haystack.length && haystack[i] !== needle[j]) {
+      if (j !== 0) {
+        j = lps[j - 1];
+      } else {
+        i++;
+      }
+    }
+  }
+
+  return -1;
+}
+
+function buildLPS(pattern) {
+  const lps = new Array(pattern.length).fill(0);
+
+  let length = 0;
+  let i = 1;
+
+  while (i < pattern.length) {
+    if (pattern[i] === pattern[length]) {
+      length++;
+      lps[i] = length;
+      i++;
+    } else {
+      if (length !== 0) {
+        length = lps[length - 1];
+      } else {
+        lps[i] = 0;
+        i++;
+      }
+    }
+  }
+
+  return lps;
+}
